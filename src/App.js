feb319e420103b2708cf2109124285601b98e006
Haoran Zhou <kens0serious@gmail.com>
2022-02-10 21:57:55 -0500

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { useState, useEffect } from 'react'
 import { FaSearch } from 'react-icons/fa'
 import Photo from './Photo'
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 const mainUrl = `https://api.unsplash.com/photos/`
 const searchUrl = `https://api.unsplash.com/search/photos/`
 
 function App() {
+  const [loading, setLoading] = useState(false)
+  const fetchImages = async () => {
+    setLoading(true)
+    let url
+    url = `${mainUrl}${clientID}`
+    try {
+      const res = await fetch(url)
+      const data = await res.json()
+      setLoading(false)
+      console.log(data)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchImages()
+  }, [])
+
   return <h2>stock photos starter</h2>
 }
 
